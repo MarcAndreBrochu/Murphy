8818c7f2e509aff0247ec8221bec585024e3c85e
Marc-André Brochu <marcandrebrochu@gmail.com>
2016-04-06 14:52:32 -0400

    Added welcome message :D

--- murphy/src/kernel.c
+++ murphy/src/kernel.c
@@ -1,6 +1,10 @@
+#include <kprintf.h>
+
 #define WAIT_FOREVER() while (1) {}
 
 void kmain() {
+
+    kprintf("Hello from kernel! :)\n");
 
     // Put the kernel in idle (since kmain should not return)
     WAIT_FOREVER();
