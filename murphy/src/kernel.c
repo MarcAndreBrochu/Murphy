fd80bcc6df7b3fb728fa5bf68df0f57b671490f1
Marc-André Brochu <marcandrebrochu@gmail.com>
2016-04-06 21:01:52 -0400

    Added tests for kprint_hex functions

--- murphy/src/kernel.c
+++ murphy/src/kernel.c
@@ -8,6 +8,19 @@ void kmain() {
     kterm_initialize();
     kprint("Hello from kernel! :)\n");
 
+    kprint_hexu(0x1);
+    kprint("\n");
+    kprint_hexu(0x10);
+    kprint("\n");
+    kprint_hexu(0xcafe);
+    kprint("\n");
+    kprint_hexu(0xcafebabe);
+    kprint("\n");
+    kprint_hexu(3735928559);
+    kprint("\n");
+    kprint_hexi(-70);
+    kprint("\n");
+
     // Put the kernel in idle (since kmain should not return)
     WAIT_FOREVER();
 }
